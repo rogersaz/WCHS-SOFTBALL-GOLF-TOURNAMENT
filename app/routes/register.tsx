743a5fbd3417d14ce917f86a746978dc8e8cfb95
Mark <45799393+rogersaz@users.noreply.github.com>
2024-09-09 16:17:45 -0700

    Update register.tsx

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -159,7 +159,7 @@ export default function SponsorshipForm() {
                       type="submit" 
                       className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md"
                     >
-                      Submit Sponsorship
+                      Submit Entry
                     </button>
                   </div>
 
