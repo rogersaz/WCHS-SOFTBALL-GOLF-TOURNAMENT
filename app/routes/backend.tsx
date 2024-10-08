d58b0bc6137c596a4b1521f1594900841a92c144
Mark <45799393+rogersaz@users.noreply.github.com>
2024-10-08 07:40:08 -0700

    Update backend.tsx

--- app/routes/backend.tsx
+++ app/routes/backend.tsx
@@ -10,8 +10,8 @@ interface Registration {
   team_name: string
   email: string
   phone: string
-  'pay-with-check': boolean
-  'pay-with-cc': boolean
+  'pay_with_check': boolean
+  'pay_with_cc': boolean
 }
 
 interface PenciledIn {
